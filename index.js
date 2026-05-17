09d02a047ab6798d5b3b5c75a534d78dbd474ef0
Reshma-0654 <shaikreshma06542@gmail.com>
2026-05-17 20:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3003, () => {
+  console.log("Server running on http://localhost:3003");
 });
-
